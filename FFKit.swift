367d79dc202d70c7a7438355b1982799baff8298
fastSwift <adri_labbe@hotmail.com>
2017-09-25 17:58:31 -0300

    FFKit can now print URLS

--- FFKit.swift
+++ FFKit.swift
@@ -23,45 +23,49 @@ class FFKit {
 		print("Hide activity")
 	}
 
-    static func createAppDir() {
-        if !FileManager.default.fileExists(atPath: FFKit.appDir) {
-            system("mkdir \(FFKit.appDir)")
-        }
-    }
-    
-    static var appDir: String {
-        return "/home/swiftexec/.db/\(FFKit.uuid)/"
-    }
-    
-    static func contentsOf(dir path:String) -> [String] {
-        var files = [String]()
-        if FileManager.default.fileExists(atPath: path) {
-            for file in try! FileManager.default.contentsOfDirectory(atPath: FFKit.appDir) {
-                files.append(appDir+"/"+file)
-            }
-        }
-        
-        return files
-    }
-    
-    static func contentsOf(file path:String) -> Data? {
-        return FileManager.default.contents(atPath: path)
-    }
-    
-    static func create(dir path: String) {
-        system("mkdir '\(path)'")
-    }
-    
-    static func create(file path: String) {
-        system("touch '\(path)'")
-    }
-    
-    static func remove(item path: String) {
-        system("rm -rf '\(path)'")
-    }
-    
-    static func write(text: String, toFile file:String, append: Bool) throws {
-        try text.write(to: URL(fileURLWithPath:file), atomically: append, encoding: String.Encoding.utf8)
-    }
+	static func createAppDir() {
+        	if !FileManager.default.fileExists(atPath: FFKit.appDir) {
+            		system("mkdir \(FFKit.appDir)")
+        	}
+    	}
+
+    	static var appDir: String {
+        	return "/home/swiftexec/.db/\(FFKit.uuid)/"
+    	}
+
+    	static func contentsOf(dir path:String) -> [String] {
+        	var files = [String]()
+        	if FileManager.default.fileExists(atPath: path) {
+            		for file in try! FileManager.default.contentsOfDirectory(atPath: FFKit.appDir) {
+                		files.append(appDir+"/"+file)
+            		}
+       		}
+
+        	return files
+    	}
+
+    	static func contentsOf(file path:String) -> Data? {
+        	return FileManager.default.contents(atPath: path)
+    	}
+
+    	static func create(dir path: String) {
+        	system("mkdir '\(path)'")
+    	}
+
+    	static func create(file path: String) {
+        	system("touch '\(path)'")
+    	}
+
+    	static func remove(item path: String) {
+        	system("rm -rf '\(path)'")
+    	}
+
+    	static func write(text: String, toFile file:String, append: Bool) throws {
+        	try text.write(to: URL(fileURLWithPath:file), atomically: append, encoding: String.Encoding.utf8)
+    	}
+
+    	static func printURL(_ url: URL) {
+		print("<URL>\(url.absoluteString)</URL>")
+    	}
 
 }
